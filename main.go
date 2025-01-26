5f4be4205a4e0317d5a309c9de97503cc5093f1c
chavocito <francbota94@gmail.com>
2025-01-26 22:22:32 +0000

    testing...

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("starter.....")
+	var username string
+	flag.StringVar(&username, "username", "", "input your username")
+
+	flag.Usage = func() {
+		_, err := fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		if err != nil {
+			return
+		}
+		fmt.Println("  -username string\n    input your username (required)")
+	}
+
+	// Parse the flags
+	flag.Parse()
+
+	if username == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println("Username:", username)
 }
